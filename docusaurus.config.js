7fdd41df70f53eb177c05da4e1e25fdf788777e9
zx06 <12474586+zx06@users.noreply.github.com>
2022-02-24 10:44:09 +0800

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,15 +66,6 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
             title: 'Community',
             items: [
               {
