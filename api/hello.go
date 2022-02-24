35e216665947015ab4c4a3f8eed2cb4fe66e5df5
zx06 <12474586+zx06@users.noreply.github.com>
2022-02-24 11:22:55 +0800

    add functions

---
+++ api/hello.go
@@ -0,0 +1,7 @@
+package api
+
+import "net/http"
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zx06/wiki
+
+go 1.17
